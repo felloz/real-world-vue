0006cd53a6c8a835389e63355ea6c62ae1a16812
Luis Serrano <serranol82@gmail.com>
2020-04-19 14:48:44 -0400

    Primer Commit

---
+++ src/router/index.js
@@ -0,0 +1,39 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import EventCreate from '../views/EventCreate.vue'
+import EventList from '../views/EventList'
+import EventShow from '../views/EventShow'
+import User from '../views/User'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'event-list',
+    component: EventList
+  },
+  {
+    path: '/event/:id',
+    name: 'event-show',
+    component: EventShow
+  },
+  {
+    path: '/event/create',
+    name: 'event-create',
+    component: EventCreate
+  },
+  {
+    path: '/user/:username',
+    name: 'user',
+    component: User,
+    props: true
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+})
+
+export default router
